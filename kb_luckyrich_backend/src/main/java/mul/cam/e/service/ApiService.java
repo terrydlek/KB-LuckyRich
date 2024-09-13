f913c3aaeddbfb64f5dd3ac5bc1b166490dbcb78
siyun2 <tlfqjans37@gmail.com>
2024-09-13 17:52:09 +0900

    fix: kakaoApiLogin
    카카오 로그인 api 수정

--- kb_luckyrich_backend/src/main/java/mul/cam/e/service/ApiService.java
+++ kb_luckyrich_backend/src/main/java/mul/cam/e/service/ApiService.java
@@ -71,7 +71,7 @@ public class ApiService {
     }
 
     @Value("${kakao.oauth.client-id}") String KakaoClientId;
-    @Value("${kako.oauth.url}") String KakoUrl;
+    @Value("${kakao.oauth.url}") String KakaoUrl;
     private final String kakao_token_url = "https://kauth.kakao.com/oauth/token";
 
     public KakaoResponseDto getKakaoToken(String code) {
@@ -86,12 +86,11 @@ public class ApiService {
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", KakaoClientId); // 본인의 REST API 키로 교체
-        params.add("redirect_uri", KakoUrl); // 리다이렉트 URI
+        params.add("redirect_uri", KakaoUrl); // 리다이렉트 URI
         params.add("code", code); // Authorization Code
 
         // HttpEntity에 헤더와 파라미터 담기
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
-
         // 카카오 서버로 요청 전송
         ResponseEntity<KakaoResponseDto> response = restTemplate.postForEntity(kakao_token_url, request, KakaoResponseDto.class);
 
