ad67eef1a1a4de54d5abc20f450e370aff4f0bdc
terrydlek <terryf6188@gmail.com>
2024-09-17 22:14:46 +0900

    modify SecurityConfig, JwtTokenFilter

--- kb_luckyrich_backend/src/main/java/mul/cam/e/controller/ApiController.java
+++ kb_luckyrich_backend/src/main/java/mul/cam/e/controller/ApiController.java
@@ -11,6 +11,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
--- kb_luckyrich_backend/src/main/java/mul/cam/e/dto/DepositDto.java
+++ kb_luckyrich_backend/src/main/java/mul/cam/e/dto/DepositDto.java
@@ -10,13 +10,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DepositDto {
-    private String 금융회사;
-    private String 상품명;
-    private String 세전이자율;
-    private String 세후이자율;
-    private String 세후이자;
-    private String 최고우대금리;
-    private String 가입제한여부;
-    private String 이자계산방식;
+    private String company;
+    private String prodname;
+    private String prerate;
+    private String afterrate;
+    private String afterinterest;
+    private String bestinterest;
+    private String limit;
+    private String calmethod;
 }
-
