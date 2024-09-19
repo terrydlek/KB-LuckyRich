4c68d6368490cdd91822661a76feb62d27e60426
kimjaei <ksy980417@gmail.com>
2024-09-19 09:21:29 +0900

    feat : add recomendation page
    안정추구형, 위험중립형 펀드 추천 페이지 추가

--- kb_luckyrich_backend/src/main/java/mul/cam/e/dto/FundDto.java
+++ kb_luckyrich_backend/src/main/java/mul/cam/e/dto/FundDto.java
@@ -4,18 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// 펀드 데이터를 표현하는 Data Transfer Object 클래스
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class FundDto {
-    private String managementCompany;
-    private String productName;
-    private double oneAccu;
-    private double threeAccu;
-    private double sixAccu;
-    private double twelveAccu;
-    private int fundRank;
-    private String fundType;
-    private int advanceFee;
-    private double terFee;
+//    private String managementCompany;
+//    private String productName;
+//    private double oneAccu;
+//    private double threeAccu;
+//    private double sixAccu;
+//    private double twelveAccu;
+//    private int fundRank;
+//    private String fundType;
+//    private int advanceFee;
+//    private double terFee;
+
+    private String name;
+    private String symbol;
+    private String lastPrice;
+    private String changePercent;
+    private String totalAssets;
+    private String lastUpdate;
+    private String country;
+    private String issuer;
+    private int riskRating;
+    private String assetClass;
 }
