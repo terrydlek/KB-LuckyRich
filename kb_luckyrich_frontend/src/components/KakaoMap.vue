54fc01f0027724b2761a141e3a21b053dce4a28f
siyun2 <tlfqjans37@gmail.com>
2024-09-25 13:42:09 +0900

    fix : kakaomap.vue appkey가져오기
    - .env 파일에서 appkey 가져오기

--- kb_luckyrich_frontend/src/components/KakaoMap.vue
+++ kb_luckyrich_frontend/src/components/KakaoMap.vue
@@ -89,9 +89,8 @@ export default {
                 script.onerror = () => {
                     console.error("Failed to load Kakao Map script");
                 };
-                const kakaoApiKey = process.env.VUE_APP_KAKAO_MAP_APPKEY;
-                script.src = `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${kakaoApiKey}&libraries=services`;
-                document.head.appendChild(script);
+                const kakaoApiKey = import.meta.env.VITE_KAKAO_MAP_APPKEY;
+                script.src = `https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${kakaoApiKey}&libraries=services`;                document.head.appendChild(script);
             }
         },
         initKakaoMap() {
