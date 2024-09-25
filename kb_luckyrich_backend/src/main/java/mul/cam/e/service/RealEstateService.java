2d1577e6eb5c4381b080c90934283369dbef6b36
siyun2 <tlfqjans37@gmail.com>
2024-09-25 17:35:13 +0900

    fix : realestateservice 수정
    - realestateservice에 redis기능 추가

--- kb_luckyrich_backend/src/main/java/mul/cam/e/service/RealEstateService.java
+++ kb_luckyrich_backend/src/main/java/mul/cam/e/service/RealEstateService.java
@@ -11,13 +11,23 @@ import java.util.List;
 @Transactional
 public class RealEstateService {
     private final RealEstateDao realEstateDao;
+    private final RedisService redisService;
 
-    public RealEstateService(RealEstateDao realEstateDao) {
+    public RealEstateService(RealEstateDao realEstateDao, RedisService redisService) {
         this.realEstateDao = realEstateDao;
+        this.redisService = redisService;
     }
 
 
     public List<RealEstateDto> getEstate() {
-        return realEstateDao.getRealEstates();
+        String redisKey = "realestateList";
+        List<RealEstateDto> estateList = redisService.getRealEstateData(redisKey);
+        if (estateList == null || estateList.isEmpty()) {
+            // Redis에 데이터가 없을 경우 데이터베이스에서 조회
+            estateList = realEstateDao.getRealEstates();
+            // 조회한 데이터를 Redis에 캐싱 (유효 기간: 5분)
+            redisService.setData(redisKey, estateList, 100);
+        }
+        return estateList;
     }
 }
