cd46c98cb5003c12b3b9418822013da134c2b4cc
kimjaei <ksy980417@gmail.com>
2024-10-02 13:19:06 +0900

    chore : modify infinite-scroll
    - 무한 스크롤에서 페이지네이션(10개씩)으로 변경

--- kb_luckyrich_frontend/src/router/account/account.js
+++ kb_luckyrich_frontend/src/router/account/account.js
@@ -18,6 +18,7 @@ import StockDetail from '@/components/account/details/StockDetail.vue';
 import Detail from '@/components/account/details/Detail.vue';
 import NewsDetail from '@/components/account/details/NewsDetail.vue';
 import AccountBook from '@/components/account/AccountBook.vue';
+import Socket from '@/views/account/Socket.vue';
 
 export default [
   {
@@ -119,5 +120,10 @@ export default [
     path: '/luckyrich/accountbook',
     name: 'accountBook',
     component: AccountBook,
+  },
+  {
+    path: '/luckyrich/portfolio',
+    name: 'Socket',
+    component: Socket,
   },
 ];
--- kb_luckyrich_frontend/src/views/account/Socket.vue
+++ kb_luckyrich_frontend/src/views/account/Socket.vue
@@ -1,31 +1,32 @@
+<!-- 포트폴리오 PDF 내용 -->
 <template>
-    <div>
-        <div id="element-to-print" style="padding: 20px; border: 1px solid #ccc;">
-            <h1>PDF 테스트</h1>
-            <p>asdsa</p>
-            <p>dwqoijcqwdiojcwdqoijcdwqoijo</p>
-        </div>
-        <button @click="generatePDF">PDF 다운로드</button>
+  <div>
+    <div id="element-to-print" style="padding: 20px; border: 1px solid #ccc">
+      <h1>PDF 테스트</h1>
+      <p>asdsa</p>
+      <p>dwqoijcqwdiojcwdqoijcdwqoijo</p>
     </div>
+    <button @click="generatePDF">PDF 다운로드</button>
+  </div>
 </template>
 
 <script>
-import html2pdf from "html2pdf.js";
+import html2pdf from 'html2pdf.js';
 
 export default {
-    methods: {
-        generatePDF() {
-            const element = document.getElementById("element-to-print");
-            html2pdf()
-                .from(element)
-                .save("download.pdf")
-                .then(() => {
-                    console.log("PDF가 다운로드되었습니다.");
-                })
-                .catch((error) => {
-                    console.error("PDF 다운로드 중 오류 발생:", error);
-                });
-        },
+  methods: {
+    generatePDF() {
+      const element = document.getElementById('element-to-print');
+      html2pdf()
+        .from(element)
+        .save('download.pdf')
+        .then(() => {
+          console.log('PDF가 다운로드되었습니다.');
+        })
+        .catch((error) => {
+          console.error('PDF 다운로드 중 오류 발생:', error);
+        });
     },
+  },
 };
 </script>
