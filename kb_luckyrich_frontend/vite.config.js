7984e40ab7b1970ee2c981be3172baca86e40a02
kimjaei <ksy980417@gmail.com>
2024-10-11 16:35:43 +0900

    chore : modify dark-mode icon

--- kb_luckyrich_frontend/vite.config.js
+++ kb_luckyrich_frontend/vite.config.js
@@ -4,15 +4,17 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   define: {
     global: 'window', // global을 window로 정의합니다.
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
